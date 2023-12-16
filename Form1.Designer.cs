ef6a6bc01da4a350e2579f363c65725dac3ad0ea
hp <hp@DESKTOP-44PKIDL>
2023-12-16 19:13:47 +0400

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace Kassa_Aparati
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            button2 = new Button();
+            groupBox1 = new GroupBox();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            textBox2 = new TextBox();
+            listBox1 = new ListBox();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(62, 30);
+            label1.Name = "label1";
+            label1.Size = new Size(74, 41);
+            label1.TabIndex = 0;
+            label1.Text = "Kod";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 80);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(173, 47);
+            textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.White;
+            button1.Location = new Point(191, 21);
+            button1.Name = "button1";
+            button1.Size = new Size(145, 50);
+            button1.TabIndex = 2;
+            button1.Text = "Kodlar";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.White;
+            button2.Location = new Point(191, 77);
+            button2.Name = "button2";
+            button2.Size = new Size(145, 50);
+            button2.TabIndex = 3;
+            button2.Text = "Əlavə et";
+            button2.UseVisualStyleBackColor = false;
+            button2.Click += button2_Click;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(button12);
+            groupBox1.Controls.Add(button13);
+            groupBox1.Controls.Add(button14);
+            groupBox1.Controls.Add(button15);
+            groupBox1.Controls.Add(button8);
+            groupBox1.Controls.Add(button9);
+            groupBox1.Controls.Add(button10);
+            groupBox1.Controls.Add(button11);
+            groupBox1.Controls.Add(button7);
+            groupBox1.Controls.Add(button6);
+            groupBox1.Controls.Add(button5);
+            groupBox1.Controls.Add(button4);
+            groupBox1.Controls.Add(button3);
+            groupBox1.Controls.Add(textBox2);
+            groupBox1.Location = new Point(40, 249);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(427, 332);
+            groupBox1.TabIndex = 4;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Ödənilən";
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.White;
+            button12.Location = new Point(297, 250);
+            button12.Name = "button12";
+            button12.Size = new Size(67, 47);
+            button12.TabIndex = 17;
+            button12.Text = "0";
+            button12.UseVisualStyleBackColor = false;
+            button12.Click += button12_Click;
+            // 
+            // button13
+            // 
+            button13.BackColor = Color.White;
+            button13.Location = new Point(224, 250);
+            button13.Name = "button13";
+            button13.Size = new Size(67, 47);
+            button13.TabIndex = 18;
+            button13.Text = "9";
+            button13.UseVisualStyleBackColor = false;
+            button13.Click += button13_Click;
+            // 
+            // button14
+            // 
+            button14.BackColor = Color.White;
+            button14.Location = new Point(151, 250);
+            button14.Name = "button14";
+            button14.Size = new Size(67, 47);
+            button14.TabIndex = 16;
+            button14.Text = "8";
+            button14.UseVisualStyleBackColor = false;
+            button14.Click += button14_Click;
+            // 
+            // button15
+            // 
+            button15.BackColor = Color.White;
+            button15.Location = new Point(77, 250);
+            button15.Name = "button15";
+            button15.Size = new Size(67, 47);
+            button15.TabIndex = 15;
+            button15.Text = "7";
+            button15.UseVisualStyleBackColor = false;
+            button15.Click += button15_Click;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.White;
+            button8.Location = new Point(297, 197);
+            button8.Name = "button8";
+            button8.Size = new Size(67, 47);
+            button8.TabIndex = 13;
+            button8.Text = ",";
+            button8.UseVisualStyleBackColor = false;
+            button8.Click += button8_Click;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.White;
+            button9.Location = new Point(224, 197);
+            button9.Name = "button9";
+            button9.Size = new Size(67, 47);
+            button9.TabIndex = 14;
+            button9.Text = "6";
+            button9.UseVisualStyleBackColor = false;
+            button9.Click += button9_Click;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.White;
+            button10.Location = new Point(151, 197);
+            button10.Name = "button10";
+            button10.Size = new Size(67, 47);
+            button10.TabIndex = 12;
+            button10.Text = "5";
+            button10.UseVisualStyleBackColor = false;
+            button10.Click += button10_Click;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.White;
+            button11.Location = new Point(77, 197);
+            button11.Name = "button11";
+            button11.Size = new Size(67, 47);
+            button11.TabIndex = 11;
+            button11.Text = "4";
+            button11.UseVisualStyleBackColor = false;
+            button11.Click += button11_Click;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.White;
+            button7.Font = new Font("Segoe UI", 10.2F, FontStyle.Bold, GraphicsUnit.Point);
+            button7.Location = new Point(297, 144);
+            button7.Name = "button7";
+            button7.Size = new Size(67, 47);
+            button7.TabIndex = 10;
+            button7.Text = "Enter";
+            button7.UseVisualStyleBackColor = false;
+            button7.Click += button7_Click;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.White;
+            button6.Location = new Point(224, 144);
+            button6.Name = "button6";
+            button6.Size = new Size(67, 47);
+            button6.TabIndex = 10;
+            button6.Text = "3";
+            button6.UseVisualStyleBackColor = false;
+            button6.Click += button6_Click;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.White;
+            button5.Location = new Point(151, 144);
+            button5.Name = "button5";
+            button5.Size = new Size(67, 47);
+            button5.TabIndex = 9;
+            button5.Text = "2";
+            button5.UseVisualStyleBackColor = false;
+            button5.Click += button5_Click;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.White;
+            button4.Location = new Point(78, 144);
+            button4.Name = "button4";
+            button4.Size = new Size(67, 47);
+            button4.TabIndex = 8;
+            button4.Text = "1";
+            button4.UseVisualStyleBackColor = false;
+            button4.Click += button4_Click;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.White;
+            button3.Location = new Point(298, 71);
+            button3.Name = "button3";
+            button3.Size = new Size(67, 47);
+            button3.TabIndex = 7;
+            button3.Text = "X";
+            button3.UseVisualStyleBackColor = false;
+            button3.Click += button3_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(77, 71);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(195, 47);
+            textBox2.TabIndex = 6;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 41;
+            listBox1.Location = new Point(485, 249);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(711, 332);
+            listBox1.TabIndex = 5;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(593, 30);
+            label2.Name = "label2";
+            label2.Size = new Size(244, 41);
+            label2.TabIndex = 6;
+            label2.Text = "Toplam məbləğ:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(569, 83);
+            label3.Name = "label3";
+            label3.Size = new Size(268, 41);
+            label3.TabIndex = 7;
+            label3.Text = "Ödənilən məbləğ:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(621, 142);
+            label4.Name = "label4";
+            label4.Size = new Size(216, 41);
+            label4.TabIndex = 8;
+            label4.Text = "Qalıq məbləğ:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(906, 30);
+            label5.Name = "label5";
+            label5.Size = new Size(106, 41);
+            label5.TabIndex = 9;
+            label5.Text = "0 AZN";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(906, 86);
+            label6.Name = "label6";
+            label6.Size = new Size(106, 41);
+            label6.TabIndex = 10;
+            label6.Text = "0 AZN";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(906, 142);
+            label7.Name = "label7";
+            label7.Size = new Size(106, 41);
+            label7.TabIndex = 11;
+            label7.Text = "0 AZN";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(18F, 41F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.WindowFrame;
+            ClientSize = new Size(1245, 589);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(listBox1);
+            Controls.Add(groupBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 18F, FontStyle.Bold, GraphicsUnit.Point);
+            Margin = new Padding(7, 6, 7, 6);
+            Name = "Form1";
+            Text = " Kassa";
+            Load += Form1_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private Button button2;
+        private GroupBox groupBox1;
+        private Button button4;
+        private Button button3;
+        private TextBox textBox2;
+        private ListBox listBox1;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button7;
+        private Button button6;
+        private Button button5;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,165 @@
+﻿using Kassa_Aparati.Entity;
+
+namespace Kassa_Aparati
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Product p = new Product();
+        decimal result = 0;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("1.Lays \n 2.Cola ");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] code = { "1221", "2020", "2021", "2022" };
+            string[] name = {"Lays","Coca-Cola","Rafaello","Snickers" };
+            decimal[] price = {1.50m,2.20m,5.60m,2.50m };
+            if (textBox1.Text == code[0])
+            {
+                object[] product = { p.Name = name[0], p.Price = price[0] };
+
+                foreach (var item in product)
+                {
+                    listBox1.Items.Add(item);
+                }
+                result = result + p.Price;
+            }
+           else if (textBox1.Text == code[1])
+            {
+                object[] product1 = { p.Name = name[1], p.Price = price[1] };
+
+                foreach (var item in product1)
+                {
+                    listBox1.Items.Add(item);
+                }
+                result = result + p.Price;
+
+            }
+            else if (textBox1.Text == code[2])
+            {
+                object[] product2 = { p.Name = name[2], p.Price = price[2] };
+                foreach (var item in product2)
+                {
+                    listBox1.Items.Add(item);
+
+                }
+                result=result + p.Price;
+            }
+            else if (textBox1.Text == code[3])
+            {
+                object[] product3 = { p.Name = name[3], p.Price = price[3] };
+                foreach (var item in product3)
+                {
+                    listBox1.Items.Add(item);
+
+                }
+                result = result + p.Price;
+            }
+            else
+            {
+                MessageBox.Show("Bu kod mövcud deyil");
+            }
+            label5.Text = $"{result.ToString()} azn";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Clear();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            decimal mebleg = Convert.ToDecimal(textBox2.Text);
+            if (mebleg > result)
+            {
+                label6.ForeColor = Color.Black;
+                label6.Text = $"{mebleg.ToString()} azn";
+                decimal qaliq = mebleg - result;
+                label7.Text = $"{qaliq.ToString()} azn";
+            }
+            else
+            {
+                label6.ForeColor=Color.Red;
+                label6.Text = "0.00 AZN ";
+                MessageBox.Show("Ödənilən məbləğ tutardan kiçikdir");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Button btn=(Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox2.Text += btn.Text;
+        }
+    }
+}
